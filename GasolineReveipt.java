0bc933df58624f27dd91b45415992778debb04f1
dmart030 <dmart030@ucr.edu>
2017-05-29 22:50:44 -0700

    Update 1
    First update of examples/projects related to Java

---
+++ GasolineReveipt.java
@@ -0,0 +1,13 @@
+public class GasolineReveipt {
+	public static void main( String[] args ) {
+		System.out.println( "+------------------------+" );
+		System.out.println( "|                        |" );
+		System.out.println( "|     Corner Stone       |" );
+		System.out.println( "| 2015-03-29 04:38PM     |" );
+		System.out.println( "|                        |" );
+		System.out.println( "| Gallons:      10.870   |" );
+		System.out.println( "| Price/gallon: $2.089   |" );
+		System.out.println( "|                        |" );
+		System.out.println( "+------------------------+" );
+	}
+}
